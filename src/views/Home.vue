214b011d0a1eb844387f4c1e4feff5f30b74c1e3
fiyin35 <you@example.com>
2021-09-03 14:10:19 +0100

    dataTitle component done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,59 @@
 <template>
-  <div class="home">
-    
-  </div>
+<div>
+  <main v-if="!loading">
+    <dataTitle 
+    :title="title" 
+    :dataDate="dataDate" />
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6"> 
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading"/>
+     </main>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { DataTitle } from '@/components/DataTitle'
 
 export default {
   name: 'Home',
   components: {
+    DataTitle,
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
     
-  }
+  },
+  components: {
+    
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+
+    this.dataDate = data.Data
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+
+    console.log(data)
+  },
 }
 </script>
