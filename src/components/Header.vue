09859d3906282d64499626f13b5b303e4f578e87
fiyin35 <you@example.com>
2021-09-03 08:08:20 +0100

    second commit

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div> Covid-19 tracker </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
